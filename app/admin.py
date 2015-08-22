e0e4021b59efd6fd2affd576aac53be2140d486c
hanks <zhangyuhan@nashangban.com>
2015-08-22 14:26:54 +0800

    add admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from models import User, Article, Comment, Tag, Category, Links, Ad
+
 
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'desc', 'content',)
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('click_count', 'is_recommend', 'tag', 'category',)
+        }),
+    )
+
+
+admin.site.register(User)
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Links)
+admin.site.register(Ad)
