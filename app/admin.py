7ff25a4d0c687a3b6e9bbcfb39e5ef790e114a6e
hanks <zhangyuhan@nashangban.com>
2015-08-22 14:51:34 +0800

    add kindeditor

--- app/admin.py
+++ app/admin.py
@@ -11,13 +11,20 @@ class ArticleAdmin(admin.ModelAdmin):
         }),
         ('Advanced options', {
             'classes': ('collapse',),
-            'fields': ('click_count', 'is_recommend', 'tag', 'category',)
+            'fields': ('click_count', 'is_recommend', 'tag', 'category', 'user')
         }),
     )
 
+    class Media:
+        js = (
+            '/static/js/kindeditor-4.1.10/kindeditor-min.js',
+            '/static/js/kindeditor-4.1.10/lang/zh_CN.js',
+            '/static/js/kindeditor-4.1.10/config.js',
+        )
+
 
 admin.site.register(User)
-admin.site.register(Article,ArticleAdmin)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment)
 admin.site.register(Tag)
 admin.site.register(Category)
---
+++ static/js/kindeditor-4.1.10/config.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2015/8/22.
+ */
+
+KindEditor.ready(function(K) {
+                window.editor = K.create('textarea[name=content');
+        });
