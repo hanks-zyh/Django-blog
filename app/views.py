e823b1018714abbf724d7878d6c152d03062d5e5
hanks <zhangyuhan@nashangban.com>
2015-08-23 08:36:05 +0800

    extract getPage to return a paged article_list

--- app/views.py
+++ app/views.py
@@ -11,48 +11,43 @@ logger = logging.getLogger('blog.views')
 
 # Create your views here.
 def global_setting(request):
-    return {
-        'SITE_NAME': settings.SITE_NAME,
-        'SITE_DESC': settings.SITE_DESC,
-    }
+    # 网站基本信息
+    SITE_NAME = settings.SITE_NAME
+    SITE_DESC = settings.SITE_DESC
+
+    # 导航栏分类数据
+    category_list = Category.objects.all()[:5]
+  
+
+    # 文章归档
+    # 1. 获取文章月份的归档
+    archive_list = Article.objects.distinct_date()
+    return locals()
 
 
 def index(request):
     try:
-        # 最新文章数据
-        category_list = Category.objects.all()[:5]
         article_list = Article.objects.all()
-        paginator = Paginator(article_list, 2)
-        try:
-            page = int(request.GET.get('page', 1 ))
-            article_list = paginator.page(page)
-        except (EmptyPage, InvalidPage, PageNotAnInteger):
-            article_list = paginator.page(1)
-
-        # 文章归档
-        # 1. 获取文章月份的归档
-        archive_list = Article.objects.distinct_date()
+        article_list = getPage(request, article_list)
     except Exception as e:
         logger.error(e)
     return render(request, 'index.html', locals())
 
 def archive(request):
-    try:
-        # 最新文章数据
-        category_list = Category.objects.all()[:5]
+    try: 
         year = request.GET.get('year', None)
         mouth = request.GET.get('mouth', None)
         article_list = Article.objects.filter(date_publish__icontains=year+'-'+mouth)
-        paginator = Paginator(article_list, 2)
-        try:
-            page = int(request.GET.get('page', 1 ))
-            article_list = paginator.page(page)
-        except (EmptyPage, InvalidPage, PageNotAnInteger):
-            article_list = paginator.page(1)
-
-        # 文章归档
-        # 1. 获取文章月份的归档
-        archive_list = Article.objects.distinct_date()
+        article_list = getPage(request, article_list)
     except Exception, e:
         logger.error(e)
     return render(request, 'archive.html', locals())
+
+def getPage(request, article_list):
+    paginator = Paginator(article_list, 2)
+    try:
+        page = int(request.GET.get('page', 1 ))
+        article_list = paginator.page(page)
+    except (EmptyPage, InvalidPage, PageNotAnInteger):
+        article_list = paginator.page(1)
+    return article_list
