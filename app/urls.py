97e342f9ba41eb85c80d307b54249e08cfc4f6d7
hanks <zhangyuhan@nashangban.com>
2015-08-24 12:34:32 +0800

    update

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from app.views import index, archive, tag, article
+from app.views import index, archive, tag, article, comment_post, category, do_reg, do_login, do_logout
 from django.conf.urls import  url
 urlpatterns = [
 
@@ -8,4 +8,9 @@ urlpatterns = [
     url(r'^archive/$', archive, name='archive'),
     url(r'^tag/$', tag, name='tag'),
     url(r'^article/$', article, name='article'),
+    url(r'^comment/post/$', comment_post, name='comment_post'),
+    url(r'^logout$', do_logout, name='logout'),
+    url(r'^reg', do_reg, name='reg'),
+    url(r'^login', do_login, name='login'),
+    url(r'^category/$', category, name='category'),
 ]
--- app/views.py
+++ app/views.py
@@ -18,8 +18,6 @@ def global_setting(request):
 
     # 导航栏分类数据
     category_list = Category.objects.all()[:5]
-  
-
 
     # 广告数据
     # Tag数据
@@ -29,9 +27,11 @@ def global_setting(request):
     archive_list = Article.objects.distinct_date()
 
     # 评论排行
-    comment_count_list = Comment.objects.values('article').annotate(comment_count=Count('article')).order_by('-comment_count')[:6]
-    article_comment_list = [Article.objects.get(pk=comment['article']) for comment in comment_count_list]
-    
+    comment_count_list = Comment.objects.values('article').annotate(
+        comment_count=Count('article')).order_by('-comment_count')[:6]
+    article_comment_list = [
+        Article.objects.get(pk=comment['article']) for comment in comment_count_list]
+
     # 浏览排行
     article_click_list = Article.objects.order_by('-click_count')[:6]
     # 站长推荐
@@ -53,45 +53,170 @@ def index(request):
         logger.error(e)
     return render(request, 'index.html', locals())
 
+
 def archive(request):
-    try: 
+    try:
         year = request.GET.get('year', None)
         mouth = request.GET.get('mouth', None)
-        article_list = Article.objects.filter(date_publish__icontains=year+'-'+mouth)
+        article_list = Article.objects.filter(
+            date_publish__icontains=year + '-' + mouth)
         article_list = getPage(request, article_list)
     except Exception, e:
         logger.error(e)
     return render(request, 'archive.html', locals())
 
+
 def tag(request):
     try:
-        tag_name = request.GET.get('tag',None)
+        tag_name = request.GET.get('tag', None)
         article_list = Article.objects.filter(tag__name=tag_name)
         article_list = getPage(request, article_list)
     except Exception, e:
         logger.error(e)
     return render(request, 'tag.html', locals())
 
+
 def article(request):
     try:
-        #获取文章id
+        # 获取文章id
         article_id = request.GET.get('id', None)
-        try :
+        try:
             article = Article.objects.get(pk=article_id)
         except Article.DoesNotExist:
-            return render(request, 'failure.html', {'resion':'没有找到对应文章'})
+            return render(request, 'failure.html', {'resion': '没有找到对应文章'})
+        # 评论表单
+        # comment_form = CommentForm({'author': request.user.username,
+        #                             'email': request.user.email,
+        #                             'url': request.user.url,
+        #                             'article': id} if request.user.is_authenticated() else{'article': id})
+        # 获取评论列表
+        comments = Comment.objects.filter(article=article).order_by('id')
+        comment_list = []
+        for comment in comments:
+            for item in comment_list:
+                if not hasattr(item, 'children_comment'):
+                    setattr(item, 'children_comment', [])
+                if comment.pid == item:  # 这条评论是item的子级评论
+                    item.children_comment.append(comment)
+                    break
+            if comment.pid is None:  # 父级评论
+                comment_list.append(comment)
+
     except Exception, e:
-       logger.error(e)
+        logger.error(e)
     return render(request, 'article.html', locals())
+
+
+# 提交评论
+def comment_post(request):
+    try:
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            # 获取表单信息
+            comment = Comment.objects.create(username=comment_form.cleaned_data["author"],
+                                             email=comment_form.cleaned_data[
+                                                 "email"],
+                                             url=comment_form.cleaned_data[
+                                                 "url"],
+                                             content=comment_form.cleaned_data[
+                                                 "comment"],
+                                             article_id=comment_form.cleaned_data[
+                                                 "article"],
+                                             user=request.user if request.user.is_authenticated() else None)
+            comment.save()
+        else:
+            return render(request, 'failure.html', {'reason': comment_form.errors})
+    except Exception as e:
+        logger.error(e)
+    return redirect(request.META['HTTP_REFERER'])
 
 
 def getPage(request, article_list):
     paginator = Paginator(article_list, 2)
     try:
-        page = int(request.GET.get('page', 1 ))
+        page = int(request.GET.get('page', 1))
         article_list = paginator.page(page)
     except (EmptyPage, InvalidPage, PageNotAnInteger):
         article_list = paginator.page(1)
     return article_list
 
 
+# 注销
+def do_logout(request):
+    try:
+        logout(request)
+    except Exception as e:
+        print e
+        logger.error(e)
+    return redirect(request.META['HTTP_REFERER'])
+
+# 注册
+
+
+def do_reg(request):
+    try:
+        if request.method == 'POST':
+            reg_form = RegForm(request.POST)
+            if reg_form.is_valid():
+                # 注册
+                user = User.objects.create(username=reg_form.cleaned_data["username"],
+                                           email=reg_form.cleaned_data[
+                                               "email"],
+                                           url=reg_form.cleaned_data["url"],
+                                           password=make_password(reg_form.cleaned_data["password"]),)
+                user.save()
+
+                # 登录
+                # 指定默认的登录验证方式
+                user.backend = 'django.contrib.auth.backends.ModelBackend'
+                login(request, user)
+                return redirect(request.POST.get('source_url'))
+            else:
+                return render(request, 'failure.html', {'reason': reg_form.errors})
+        else:
+            reg_form = RegForm()
+    except Exception as e:
+        logger.error(e)
+    return render(request, 'reg.html', locals())
+
+# 登录
+
+
+def do_login(request):
+    try:
+        if request.method == 'POST':
+            login_form = LoginForm(request.POST)
+            if login_form.is_valid():
+                # 登录
+                username = login_form.cleaned_data["username"]
+                password = login_form.cleaned_data["password"]
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    # 指定默认的登录验证方式
+                    user.backend = 'django.contrib.auth.backends.ModelBackend'
+                    login(request, user)
+                else:
+                    return render(request, 'failure.html', {'reason': '登录验证失败'})
+                return redirect(request.POST.get('source_url'))
+            else:
+                return render(request, 'failure.html', {'reason': login_form.errors})
+        else:
+            login_form = LoginForm()
+    except Exception as e:
+        logger.error(e)
+    return render(request, 'login.html', locals())
+
+
+def category(request):
+    try:
+        # 先获取客户端提交的信息
+        cid = request.GET.get('cid', None)
+        try:
+            category = Category.objects.get(pk=cid)
+        except Category.DoesNotExist:
+            return render(request, 'failure.html', {'reason': '分类不存在'})
+        article_list = Article.objects.filter(category=category)
+        article_list = getPage(request, article_list)
+    except Exception as e:
+        logger.error(e)
+    return render(request, 'category.html', locals())
